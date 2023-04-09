3130ae583002cbbf03d0118d2d89ede1d2dd6f9c
Yangguang Li <liyangguangcn@gmail.com>
2023-04-08 21:15:04 -0700

    Change default parameters.

--- src/gpt/index.ts
+++ src/gpt/index.ts
@@ -3,9 +3,9 @@ import start from './iteration';
 /* This is tied to the cost of the operation, choose it carefully.
  * In most cases, it either finishes in 1 or a few iterations, or won't be able to make it work,
  * so having a large value doesn't make the output better. */
-const MAX_ITERATIONS = 2;
+const MAX_ITERATIONS = 3;
 
 // Whether to log detailed process info.
-const LOG_DEBUG_INFO = true;
+const LOG_DEBUG_INFO = false;
 
 start(MAX_ITERATIONS, LOG_DEBUG_INFO);
